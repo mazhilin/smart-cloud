13346b0be4c44071ea90fa23e13bb1518fe2f3a0
marklin <2273990296@qq.com>
2023-06-22 04:44:42 +0800

    [新增功能](master)::[2023-06-21 星期三 02:17]:smart-cloud项目-platform基础模块划分

---
+++ smart-cloud-api/smart-cloud-applice-api/src/main/java/com/smart/cloud/applice/domain/logger/request/OperationLoggerRequest.java
@@ -0,0 +1,20 @@
+package com.smart.cloud.applice.domain.logger.request;
+
+import com.smart.cloud.common.domain.base.request.BaseRequest;
+
+import java.io.Serializable;
+
+/**
+ * @className: com.smart.cloud.applice.domain.logger.request.OperationLoggerRequest
+ * @projectName: 封装SmartCloud项目-OperationLoggerRequest类
+ * @module: SmartCloud项目-OperationLoggerRequest类，主要位于OperationLogger-应用操作日志模块的业务场景
+ * @content: OperationLoggerRequest类，主要用于完成应用操作日志请求实体类的封装和定义.
+ * @author: Powered by Marklin
+ * @datetime: 2023-06-22 04:07
+ * @version: 1.0.0
+ * @copyright: Copyright © 2018-2023 SmartCloud Systems Incorporated. All rights reserved.
+ */
+public class OperationLoggerRequest extends BaseRequest implements Serializable {
+
+    private static final long serialVersionUID = 4164178496014072671L;
+}
---
+++ smart-cloud-api/smart-cloud-applice-api/src/main/java/com/smart/cloud/applice/domain/logger/response/OperationLoggerResponse.java
@@ -0,0 +1,19 @@
+package com.smart.cloud.applice.domain.logger.response;
+
+import com.smart.cloud.common.domain.base.response.BaseResponse;
+
+import java.io.Serializable;
+
+/**
+ * @className: com.smart.cloud.applice.domain.logger.response.OperationLoggerResponse
+ * @projectName: 封装SmartCloud项目-OperationLoggerResponse类
+ * @module: SmartCloud项目-OperationLoggerResponse类，主要位于OperationLogger-应用操作日志响应结果模块的业务场景
+ * @content: OperationLoggerResponse类，主要用于完成应用操作日志响应结果实体的封装和定义.
+ * @author: Powered by Marklin
+ * @datetime: 2023-06-22 04:14
+ * @version: 1.0.0
+ * @copyright: Copyright © 2018-2023 SmartCloud Systems Incorporated. All rights reserved.
+ */
+public class OperationLoggerResponse extends BaseResponse implements Serializable {
+    private static final long serialVersionUID = 8709224499668890020L;
+}
