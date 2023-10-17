2d1f011791e1be995f9a00e48ab89e30d1dd304d
marklin <2273990296@qq.com>
2023-10-18 06:43:21 +0800

    [新增功能](master)::[2023-10-18 星期三 04:09]:smart-cloud项目-platform基础模块划分

---
+++ smart-cloud-domain/smart-cloud-applice-domain/src/main/java/com/smart/cloud/applice/domain/message/repository/mapper/MessageContentMapper.java
@@ -0,0 +1,14 @@
+package com.smart.cloud.applice.domain.message.repository.mapper;
+
+/**
+ * @className: com.smart.cloud.applice.domain.message.repository.mapper.MessageSceneMapper
+ * @projectName: 封装SmartCloud项目-MessageSceneMapper类
+ * @module: SmartCloud项目-MessageSceneMapper类，主要位于MessageSceneMapper模块的业务场景
+ * @content: MessageSceneMapper类，主要用于完成MessageSceneMapper的封装和定义.
+ * @author: Powered by Marklin
+ * @datetime: 2023-10-18 06:36
+ * @version: 1.0.0
+ * @copyright: Copyright © 2018-2023 SmartCloud Systems Incorporated. All rights reserved.
+ */
+public interface MessageContentMapper {
+}
