c7014b78f09f9b8ddfd3dc141f4c74ba92a26bed
marklin <2273990296@qq.com>
2023-10-18 04:52:35 +0800

    [新增功能](master)::[2023-10-18 星期三 04:09]:smart-cloud项目-platform基础模块划分

---
+++ smart-cloud-domain/smart-cloud-applice-domain/src/main/java/com/smart/cloud/applice/domain/message/object/entity/MessageFile.java
@@ -0,0 +1,16 @@
+package com.smart.cloud.applice.domain.message.object.entity;
+
+import java.io.Serializable;
+
+/**
+ * @className: com.smart.cloud.applice.domain.message.object.entity.MessageFile
+ * @projectName: 封装SmartCloud项目-MessageFile类
+ * @module: SmartCloud项目-MessageFile类，主要位于MessageFile模块的业务场景
+ * @content: MessageFile类，主要用于完成MessageFile的封装和定义.
+ * @author: Powered by Marklin
+ * @datetime: 2023-10-18 04:30
+ * @version: 1.0.0
+ * @copyright: Copyright © 2018-2023 SmartCloud Systems Incorporated. All rights reserved.
+ */
+public class MessageFile implements Serializable {
+}
---
+++ smart-cloud-domain/smart-cloud-applice-domain/src/main/java/com/smart/cloud/applice/domain/message/repository/service/impl/IgMessageFileServiceImpl.java
@@ -0,0 +1,16 @@
+package com.smart.cloud.applice.domain.message.repository.service.impl;
+
+import com.smart.cloud.applice.domain.message.repository.service.IgMessageFileService;
+
+/**
+ * @className: com.smart.cloud.applice.domain.message.repository.service.impl.IgMessageFileServiceImpl
+ * @projectName: 封装SmartCloud项目-IgMessageFileServiceImpl类
+ * @module: SmartCloud项目-IgMessageFileServiceImpl类，主要位于IgMessageFileServiceImpl模块的业务场景
+ * @content: IgMessageFileServiceImpl类，主要用于完成IgMessageFileServiceImpl的封装和定义.
+ * @author: Powered by Marklin
+ * @datetime: 2023-10-18 04:33
+ * @version: 1.0.0
+ * @copyright: Copyright © 2018-2023 SmartCloud Systems Incorporated. All rights reserved.
+ */
+public class IgMessageFileServiceImpl implements IgMessageFileService {
+}
